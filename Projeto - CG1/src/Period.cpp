b1dbaaa2a38b49319753be11d4c10424f09ae733
Unknown <up201603811@fe.up.pt>
2018-03-27 15:59:53 +0100

    Class Period

--- Projeto - CG1/src/Period.cpp
+++ Projeto - CG1/src/Period.cpp
@@ -1,8 +1,53 @@
 /*
- * Period.c
+ * Period.cpp
  *
  *  Created on: 23/03/2018
- *      Author: Francisco Miranda; João Vaz Gama Amaral; Maria Teresa Ferreira;
+ *      Author: Francisco Miranda; Joï¿½o Vaz Gama Amaral; Maria Teresa Ferreira;
  */
 
+#include 'Period.h'
 
+Period::Period(Date i, Date e, float p, string f, string s):
+		init(i), end(e), price(p), festivity(f), season(s) {}
+
+Period::Period(Date i, Date e, float p, string s):
+		init(i), end(e), price(p), festivity(f), season(s) {
+
+	this.festivity = "";
+}
+
+Date Period::getInit() const{
+	return this.init;
+}
+
+Date Period::getEnd() const{
+	return this.end;
+}
+
+float Period::getPrice() const{
+	return this.price;
+}
+
+string Period::getFestivity() const{
+	return this.festivity;
+}
+
+string Period::getSeason() const{
+	return this.season;
+}
+
+void Peroid::setInit(Date d){
+	this.init = d;
+}
+void Peroid::setEnd(Date d){
+	this.end = d;
+}
+void Peroid::setPrice(float p){
+	this.price = p;
+}
+void Peroid::setFestivity(string f){
+	this.festivity = f;
+}
+void Peroid::setSeason(string s){
+	this.season = s;
+}
--- Projeto - CG1/src/Period.h
+++ Projeto - CG1/src/Period.h
@@ -2,19 +2,32 @@
  * Period.h
  *
  *  Created on: 23/03/2018
- *      Author: Francisco Miranda; João Vaz Gama Amaral; Maria Teresa Ferreira;
+ *      Author: Francisco Miranda; Joï¿½o Vaz Gama Amaral; Maria Teresa Ferreira;
  */
 
 #ifndef SRC_PERIOD_H_
 #define SRC_PERIOD_H_
 
 class Period{
-	//dia inicio
-	//mes inicio
-	//ano inicio
-	// dia mes ano fim
-	//preço
-	//clima
+	Date init;
+	Date end;
+	float price;
+	string festivity; // easter, christmas, ...
+	string season; // winter, spring, summer,  autumn
+
+public:
+	Period(Date i, Date e, float p, string f, string s);
+	Period(Date i, Date e, float p, string s);
+	getInit() const;
+	getEnd() const;
+	getPrice() const;
+	getFestivity() const;
+	getSeason() const;
+	setInit(Date d);
+	setEnd(Date d);
+	setPrice(float p);
+	setFestivity(string f);
+	setSeason(string s);
 };
 
 
