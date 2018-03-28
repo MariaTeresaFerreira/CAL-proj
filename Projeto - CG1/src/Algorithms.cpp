a08fbe7614bdd15e5f5ce4e253aa013d585e1311
Unknown <joaovgamaamaral@gmail.com>
2018-03-28 15:00:25 +0100

    Algoritmos para odenação de vetores de destinos.
    Está a dar-me um erro na segunda função de ordenação. Não sei se devemos criar o operador igual mas não deveria ser. Se souberem feel free

--- Projeto - CG1/src/Algorithms.cpp
+++ Projeto - CG1/src/Algorithms.cpp
@@ -4,5 +4,68 @@
  *  Created on: 23/03/2018
  *      Author:Francisco Miranda; João Vaz Gama Amaral; Maria Teresa Ferreira;
  */
+#include "Algorithms.h"
 
+void bubbleSort(std::vector<Destiny*>& destiny_aux){
+	for(unsigned int j = destiny_aux.size()-1; j > 0; j--){
+		bool change = false;
+		for(unsigned int i = 0; i < j; i++)
+			if(destiny_aux[i+1]->getID() < destiny_aux[i]->getID()){
+				std::swap(destiny_aux[i], destiny_aux[i+1]);
+				change = true;
+			}
+		if (!change) return;
+	}
+}
 
+void selectionSort(std::vector<Destiny*> &d_aux){
+	unsigned int j, i;
+	std::vector<Destiny*>::iterator it;
+
+	for(i = 0; i < d_aux.size(); i++){
+
+			for(j = i+1; j < d_aux.size(); j++){
+					if(d_aux[j]->getCityName() < d_aux[i]->getCityName()){
+						Destiny* d = d_aux[i];
+						d_aux[i] = d_aux[j];
+						d_aux[j] = d;
+					}
+			}
+	}
+}
+
+int binarySearch(const std::vector<Destiny*> &d, int x)
+{
+	int left = 0, right = d.size() - 1;
+
+	while (left <= right)
+	{
+		int middle = (left + right) / 2;
+
+		if (d[middle]->getID() < x)
+			left = middle + 1;
+		else if (x < d[middle]->getID())
+			right = middle-1;
+		else
+			return middle;
+	}
+	return -1;
+}
+
+int binarySearch(const std::vector<Destiny*> &d, std::string x)
+{
+	int left = 0, right = d.size() - 1;
+
+	while (left <= right)
+	{
+		int middle = (left + right) / 2;
+
+		if (d[middle]->getCityName() < x)
+			left = middle + 1;
+		else if (x < d[middle]->getCityName())
+			right = middle-1;
+		else
+			return middle;
+	}
+	return -1;
+}
--- Projeto - CG1/src/Algorithms.h
+++ Projeto - CG1/src/Algorithms.h
@@ -8,8 +8,32 @@
 #ifndef SRC_ALGORITHMS_H_
 #define SRC_ALGORITHMS_H_
 
+#include "Libraries.h"
+#include "Destiny.h"
+/*
+ * @brief Bubble sort algorthm to sort a "Destiny" vector by ID
+ * @param destiny_aux destiny vector to be sorted
+ */
+void bubbleSort(std::vector<Destiny*>& destiny_aux);
 
+/*
+ * @brief Selection sort to sort a Destiny vector by name
+ * @param d_aux destiny vector to be sorted
+ */
+void selectionSort(std::vector<Destiny*> &d_aux);
 
+/*
+ * @brief Binary search algorithm to search a destiny by ID
+ * @param d destiny vector to search
+ * @param x destiny ID
+ */
+int binarySearch(const std::vector<Destiny*> &d, int x);
 
+/*
+ * @brief Binary search algorithm to search a destiny by name
+ * @param d destinies vector to search
+ * @param x destiny name
+ */
+int binarySearch(const std::vector<Destiny*> &d, std::string x);
 
 #endif /* SRC_ALGORITHMS_H_ */
